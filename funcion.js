d5633c12e3bfdbea735e3d1393e41425fcec690e
Cinthia iza <cizac1@est.ups.edu.ec>
2020-04-08 23:10:39 -0500

    Ultimos cambios

--- funcion.js
+++ funcion.js
@@ -1,10 +1,7 @@
-/*input type="button" id="dos" value="3" onclick="return buscar(this)" />            
-            <input type="button" id="dos" value="4" onclick="return buscar(this)" />            
-            <input type="button" id="dos" value="5" onclick="return buscar(this)" />*/
+
 
 function buscar(nombre)
 {   
-    var i=0,con;
     var primera = nombre.value;
           console.log(primera)
     var titulo=document.getElementById("titulo").value;
@@ -27,7 +24,7 @@ function buscar(nombre)
             xmlhttp.onreadystatechange=function() {
             if(this.readyState ==4 && this.status ==200){
             var data= JSON.parse(this.responseText);                
-                     
+                       
                 data.Search.forEach(movie => {
                     detalles += "<tr>"+
                     
@@ -46,10 +43,9 @@ function buscar(nombre)
                
                 }
                 
-            }; 
-            console.log("cin"+con)
-            xmlhttp.open("POST","http://www.omdbapi.com/?apikey=335d2c95&s="+ titulo +"&plot=full&rows=2&page=1",true);
-            
+            };           
+            xmlhttp.open("POST","http://www.omdbapi.com/?apikey=335d2c95&s="+ titulo +"&plot=full&page="+primera,true);
+       
             xmlhttp.send();
         }
 }
@@ -70,9 +66,9 @@ function buscar(nombre)
             xmlhttp.onreadystatechange=function() {
             if(this.readyState ==4 && this.status ==200){
                 var data= JSON.parse(this.responseText);
-                console.log(data);
+                console.log(data);               
                 var x;  
-                
+                detalles+=data.Poster;
                     for(x in data){
                         detalles+=                       
                          "<tr>"+data[x] +"</tr>" +"<br>" ;
@@ -80,7 +76,7 @@ function buscar(nombre)
                          }
                       
                
-                console.log(Object.values(data));
+                
                          
                 }
                 document.getElementById("informacion").innerHTML=detalles;
@@ -91,16 +87,55 @@ function buscar(nombre)
             xmlhttp.send();
         }
       }
-      function siguiente() {
-        if (i < len-1) {
-          i++;
-          displayCD(i);
+         
+         
+      var pagina=2;
+function siguiente()
+{   
+   pagina=pagina+1;
+    var titulo=document.getElementById("titulo").value;
+    var detalles="";
+
+    if(titulo==""){
+       
+        detalles ="<tr>" +
+        "<td  > Sin informacion </td>"+
+        "</tr>";
+        document.getElementById("informacion").innerHTML = detalles;
+        
+    }else  {
+       
+        if(window.XMLHttpRequest){
+            xmlhttp=new XMLHttpRequest();                   
+        } else{
+            xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");        
+        } 
+            xmlhttp.onreadystatechange=function() {
+            if(this.readyState ==4 && this.status ==200){
+            var data= JSON.parse(this.responseText);                
+                       
+                data.Search.forEach(movie => {
+                    detalles += "<tr>"+
+                    
+                               "<td><a href='#' onclick=\"buscarPeli('"+ movie.imdbID+"')\">Mas informacion</td>"+
+                               "<td>"+ movie.Title+"</td>"+
+                               "<td>"+ movie.Year+"</td>"+
+                               "<td>"+ movie.Type+"</td>"+
+                               "<td><img src="+ movie.Poster+"></td>"+
+                               "</tr>";
+                               
+                              
+                                                    
+                });
+              
+                document.getElementById("informacion").innerHTML=detalles;
+               
+                }
+                
+            };           
+            xmlhttp.open("POST","http://www.omdbapi.com/?apikey=335d2c95&s="+ titulo +"&plot=full&page="+pagina,true);
+       
+            xmlhttp.send();
         }
-      }
-      
-      function anterior() {
-        if (i > 0) {
-          i--;
-          displayCD(i);
-        }
-      }
+}
+   
